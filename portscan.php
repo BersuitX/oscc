3f055f4ed991d7ea2a601155c32e8818c40b0501
BersuitX <jorge.lopez.mx@gmail.com>
2018-12-26 13:30:32 -0600

    Extrae el servicio

--- portscan.php
+++ portscan.php
@@ -3,6 +3,7 @@ ini_set('max_execution_time', 0);
 ini_set('memory_limit', -1);
 
 $addr=file('legados.txt');
+$services=file('ips_legados.txt');
 
 foreach($addr as $ip) {
 list($host, $port)=explode(":",$ip);
@@ -13,6 +14,9 @@ $connection = @fsockopen(trim($host), trim($port), $errno, $errstr, 10);
   fclose($connection);
  } else {
   echo trim($host).":".trim($port)." Status: ".$errstr."\n";
+  foreach($services as $url) { 
+   print_r(preg_match("/.*trim($host).*/", $url, $match));
+  }
  }
 
 }
