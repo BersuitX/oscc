02518b4fc765a6c43512b9659ad46913b9114245
BersuitX <jorge.lopez.mx@gmail.com>
2019-03-22 18:57:52 -0600

    Update slurp.php

--- slurp.php
+++ slurp.php
@@ -1,20 +1,22 @@
 <?php
 define('BASEPATH', '.');
 include('config.php');
-print_r($config['servicios']);
+//print_r($config['servicios']);
 //array_unique($config['servicios']);
 
 foreach($config['servicios'] as $method => $servicio) {
- echo $method."\n\t".$servicio."\n";
+ echo "M&eacute;todo: ".$method."<br>";
+ echo "Servicio: ".$servicio."<br>";
 
  $ci=curl_init();
  curl_setopt($ci, CURLOPT_URL, $servicio);
- curl_setopt($ci, CURLOPT_METHOD, 'POST');
- //curl_setopt($ci, CURLOPT_RETURNTRANSFER, 0);
- $str=@curl_exec($ci);
- //curl_getinfo($ci);
+ curl_setopt($ci, CURLOPT_POST, 1);
+ //curl_setopt($ci, CURLOPT);
+ curl_setopt($ci, CURLOPT_RETURNTRANSFER, 0);
+ curl_exec($ci);
+ $curlInfo=curl_getinfo($ci);
+ echo "Tiempo total: ".$curlInfo['total_time']."<br>";
  curl_close($ci);
- sleep(2);
-
+ //sleep(2);
 }
 ?>
