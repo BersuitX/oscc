69e00c3b0712db9cd454b101a1f36b355e4bb066
BersuitX <jorge.lopez.mx@gmail.com>
2019-05-10 09:38:06 -0500

    Create loginApi.sh

---
+++ loginApi.sh
@@ -0,0 +1,1 @@
+time curl -X POST --data @Login.json -H "Accept-Enconding: deflate" -H "Content-Type: application/json" -H "X-MC-SO: okazokqaokqaok" https://apiselfservice.co/api/index.php/v1/soap/LoginUsuario.json
