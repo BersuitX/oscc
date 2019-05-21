39132cd99b56e09e40924fa24ba7ca0bbf115385
BersuitX <jorge.lopez.mx@gmail.com>
2019-05-21 16:56:51 -0500

    Archivos de pruebas

--- loadTest4.php
+++ loadTest4.php
@@ -1,6 +1,6 @@
 <?php
 $data=array();
-for($i=0; $i<50; $i++) {
+for($i=0; $i<250; $i++) {
  $data['num'.$i]=md5($i);
 }
 print_r(json_encode($data));
--- loadTest5.php
+++ loadTest5.php
@@ -1,7 +1,7 @@
 <?php
 $data=array();
 for($i=0; $i<50; $i++) {
- $data['num'.$i]=md5($i);
+ $data['num'.$i]=base64_encode($i);
 }
 print_r(json_encode($data));
 ?>
