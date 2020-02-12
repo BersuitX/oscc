d0421d10f9f81f8bfc0db00c868bfec705163082
BersuitX <jorge.lopez.mx@gmail.com>
2020-02-12 01:52:19 -0600

    Update writelogs.php
    SERVER2ENV

--- writelogs.php
+++ writelogs.php
@@ -4,5 +4,5 @@ for($i=0; $i<50; $i++) {
  $data['num'.$i]=md5($i);
 }
 error_log(json_encode($data),3,'/logs/generic/'.uniqid().$_SERVER['HOSTNAME'].'.json');
-error_log(json_encode($data),3,'/logs/'.uniqid().$_SERVER['HOSTNAME'].'.json');
+error_log(json_encode($data),3,'/logs/'.uniqid().$_ENV['HOSTNAME'].'.json');
 ?>
