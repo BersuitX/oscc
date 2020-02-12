13539ab98cdad4b01af88279d82f7954ca81397c
BersuitX <jorge.lopez.mx@gmail.com>
2020-02-12 01:54:10 -0600

    Update writelogs.php
    Idem

--- writelogs.php
+++ writelogs.php
@@ -3,6 +3,6 @@ $data=array();
 for($i=0; $i<50; $i++) {
  $data['num'.$i]=md5($i);
 }
-error_log(json_encode($data),3,'/logs/generic/'.uniqid().$_SERVER['HOSTNAME'].'.json');
+error_log(json_encode($data),3,'/logs/generic/'.uniqid().$_ENV['HOSTNAME'].'.json');
 error_log(json_encode($data),3,'/logs/'.uniqid().$_ENV['HOSTNAME'].'.json');
 ?>
